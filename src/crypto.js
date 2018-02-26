090c05eefcfc7812991ebfbe64010b26feeaea3b
Joel Douglass <joel.douglass@velir.com>
2018-02-26 11:31:40 -0500

    Enhance cryptography functions to encrypt-THEN-mac a JSON payload. Decoding validates the content and returns an Either of the payload.

--- src/crypto.js
+++ src/crypto.js
@@ -1,39 +1,105 @@
-// Evolved from: https://gist.github.com/vlucas/2bd40f62d20c1d49237a109d491974eb
+// Evolved from:
+//  https://gist.github.com/vlucas/2bd40f62d20c1d49237a109d491974eb
+//  AND
+//  https://github.com/mozilla/node-client-sessions
+
 const crypto = require('crypto');
 const R = require('ramda');
+const Either = require('data.either');
+const base64 = require('base64url');
 
-const IV_LENGTH = 16; // For AES, this is always 16
+const encryptionAlgorithm = 'aes-256-cbc';
+const hmacAlgorithm = 'sha256';
 
-const encrypt = R.curry((key, text) => {
+// IV length in Bytes
+const ivLength = 16;
+
+const encrypt = R.curry((key, iv, plainText) => {
 
   if(!key || key === ''){
     throw new Error('Encryption key not defined');
   }
 
-  const iv = crypto.randomBytes(IV_LENGTH);
-  const cipher = crypto.createCipheriv('aes-256-cbc', new Buffer(key), iv);
+  const cipher = crypto.createCipheriv(
+    encryptionAlgorithm,
+    new Buffer(key),
+    iv);
 
-  let encrypted = cipher.update(text);
+  let encrypted = cipher.update(plainText);
   encrypted = Buffer.concat([encrypted, cipher.final()]);
 
-  return iv.toString('base64') + ':' + encrypted.toString('base64');
+  return encrypted;
 });
 
-const decrypt = R.curry((key, text) => {
+const computeHmac = R.curry((key, iv, cipherText) => {
 
   if(!key || key === ''){
-    throw new Error('Encryption key not defined');
+    throw new Error('Validation key not defined');
   }
 
-  const textParts = text.split(':');
-  const iv = new Buffer(textParts.shift(), 'base64');
-  const encryptedText = new Buffer(textParts.join(':'), 'base64');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', new Buffer(key), iv);
+  const hmacAlg = crypto.createHmac(hmacAlgorithm, key);
 
-  let decrypted = decipher.update(encryptedText);
-  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  hmacAlg.update(iv);
+  hmacAlg.update('.');
+  hmacAlg.update(cipherText);
 
-  return decrypted.toString();
+  return hmacAlg.digest();
 });
 
-module.exports = { decrypt, encrypt };
+const encode = R.curry((encryptionKey, validationKey, payload) => {
+
+  const iv = crypto.randomBytes(16);
+  const plainText = new Buffer(JSON.stringify(payload), 'utf8');
+  const cipherText = encrypt(encryptionKey, iv, plainText);
+  const hmac = computeHmac(validationKey, iv, cipherText);
+
+  return [
+    base64.encode(iv),
+    base64.encode(cipherText),
+    base64.encode(hmac)
+  ].join('.');
+});
+
+const decode = R.curry((encryptionKey, validationKey, content) => {
+
+  const components = content.split('.');
+
+  if(components.length !== 3){
+    return Either.Left('Invalid content. Must be in format IV.Ciphertext.HMAC');
+  }
+
+  let iv, cipherText, hmac;
+
+  try {
+    [iv, cipherText, hmac] = components.map(c => base64.toBuffer(c));
+  } catch(e){
+    return Either.Left('Invalid content. Could not base64 decode components.');
+  }
+
+  if(iv.length !== ivLength){
+    return Either.Left('Invalid content. IV is incorrect length.');
+  }
+
+  const expectedHmac = computeHmac(validationKey, iv, cipherText);
+
+  if(!crypto.timingSafeEqual(expectedHmac, hmac)){
+    return Either.Left('Invalid HMAC.');
+  }
+
+  const decipher = crypto.createDecipheriv(
+    encryptionAlgorithm,
+    new Buffer(encryptionKey),
+    iv);
+
+  let plainText = decipher.update(cipherText, 'binary', 'utf8');
+  plainText += decipher.final('utf8');
+
+  try {
+    return Either.Right(JSON.parse(plainText));
+  }
+  catch(e){
+    return Either.Left('Invalid JSON payload: ${plainText}');
+  }
+});
+
+module.exports = { encode, decode };
--- test/cryptoTest.js
+++ test/cryptoTest.js
@@ -1,38 +1,162 @@
+const R = require('ramda');
 const {expect} = require('chai');
 const crypto = require('../src/crypto');
 var randomstring = require('randomstring');
 
 describe('Crypto', function(){
 
-  it('should encrypt and decrypt successfully', function(){
-    const text = 'foobar';
-    const key = randomstring.generate(32);
-    const encrypted = crypto.encrypt(key, text);
-    const decrypted = crypto.decrypt(key, encrypted);
+  const encoderDecoder = () => {
+    const encryptKey = randomstring.generate(32);
+    const validationKey = randomstring.generate(32);
+    return [
+      crypto.encode(encryptKey, validationKey),
+      crypto.decode(encryptKey, validationKey)
+    ];
+  };
 
-    expect(decrypted).to.equal(text);
-    expect(encrypted).to.not.equal(text);
+  it('should encode and decode successfully', function(){
+
+    const payload = 'foobar';
+    const [encode, decode] = encoderDecoder();
+
+    const encoded = encode(payload);
+    const decoded = decode(encoded);
+
+    expect(decoded.isRight).to.be.true;
+    expect(decoded.get()).to.equal(payload);
+    expect(encoded).to.not.equal(payload);
   });
 
-  it('encrypted values should be seeded with random IV', function(){
+  it('should encode and decode objects', function(){
+
+    const payload = {
+      foo: 'bar'
+    };
+
+    const [encode, decode] = encoderDecoder();
+
+    const encoded = encode(payload);
+    const decoded = decode(encoded);
+
+    expect(decoded.isRight).to.be.true;
+    expect(decoded.get().foo).to.equal('bar');
+  });
+
+  it('encoded values should be seeded with random IV', function(){
 
     const text = 'foobar';
-    const key = randomstring.generate(32);
+    const [encode1] = encoderDecoder();
+    const [encode2] = encoderDecoder();
 
-    const encrypted1 = crypto.encrypt(key, text);
-    const encrypted2 = crypto.encrypt(key, text);
+    const encoded1 = encode1(text);
+    const encoded2 = encode2(text);
 
-    expect(encrypted1).to.not.equal(encrypted2);
+    expect(encoded1).to.not.equal(encoded2);
   });
 
   it('encrypt should throw for missing key', function(){
-    expect(() => crypto.encrypt(null, 'foo')).to.throw(/Encryption key not defined/);
-    expect(() => crypto.encrypt('', 'foo')).to.throw(/Encryption key not defined/);
+
+    const validationKey = randomstring.generate(32);
+
+    expect(() => crypto.encode(null, validationKey, 'foo')).to.throw(/Encryption key not defined/);
+    expect(() => crypto.encode('', validationKey, 'foo')).to.throw(/Encryption key not defined/);
   });
 
-  it('encrypt should throw for missing key', function(){
-    expect(() => crypto.decrypt(null, 'foo')).to.throw(/Encryption key not defined/);
-    expect(() => crypto.decrypt('', 'foo')).to.throw(/Encryption key not defined/);
+  it('hmac should throw for missing key', function(){
+
+    const cryptoKey = randomstring.generate(32);
+
+    expect(() => crypto.encode(cryptoKey, null, 'foo')).to.throw(/Validation key not defined/);
+    expect(() => crypto.encode(cryptoKey, '', 'foo')).to.throw(/Validation key not defined/);
   });
 
+  it('should error for bad encoded values structure', function(){
+
+    const payload = 'foobar';
+    const [encode, decode] = encoderDecoder();
+
+    // Encode, then lop off the last part
+    const encoded = encode(payload)
+      .split('.')
+      .slice(0, 2)
+      .join('.');
+
+    const decoded = decode(encoded);
+
+    expect(decoded.isLeft).to.be.true;
+    expect(decoded.merge()).to.match(/Invalid content. Must be in format IV.Ciphertext.HMAC/);
+  });
+
+  it('should error for bad iv', function(){
+
+    const payload = 'foobar';
+    const [encode, decode] = encoderDecoder();
+
+    // Encode, then lop off the last part
+    let encoded = encode(payload);
+    // Inject non base64 character
+    encoded = encoded.substring(2,encoded.length);
+
+    const decoded = decode(encoded);
+
+    expect(decoded.isLeft).to.be.true;
+    expect(decoded.merge()).to.match(/Invalid content. IV is incorrect length./);
+  });
+
+  it('should error for tampered hmac', function(){
+
+    const payload = 'foobar';
+    const [encode, decode] = encoderDecoder();
+
+    // Encode, then lop off the last part
+    const encoded = encode(payload);
+
+    const [iv, ct, hmac] = encoded.split('.');
+
+    const badHmac = R.repeat('a', hmac.length).join('');
+    const newEncoded = [iv, ct, badHmac].join('.');
+
+    const decoded = decode(newEncoded);
+
+    expect(decoded.isLeft).to.be.true;
+    expect(decoded.merge()).to.match(/Invalid HMAC./);
+  });
+
+  it('should error for tampered ciphertext', function(){
+
+    const payload = 'foobar';
+    const [encode, decode] = encoderDecoder();
+
+    // Encode, then lop off the last part
+    const encoded = encode(payload);
+
+    const [iv, ct, hmac] = encoded.split('.');
+
+    const badCt = R.repeat('a', ct.length).join('');
+    const newEncoded = [iv, badCt, hmac].join('.');
+
+    const decoded = decode(newEncoded);
+
+    expect(decoded.isLeft).to.be.true;
+    expect(decoded.merge()).to.match(/Invalid HMAC./);
+  });
+
+  it('should error for tampered iv', function(){
+
+    const payload = 'foobar';
+    const [encode, decode] = encoderDecoder();
+
+    // Encode, then lop off the last part
+    const encoded = encode(payload);
+
+    const [iv, ct, hmac] = encoded.split('.');
+
+    const badIv = R.repeat('a', iv.length).join('');
+    const newEncoded = [badIv, ct, hmac].join('.');
+
+    const decoded = decode(newEncoded);
+
+    expect(decoded.isLeft).to.be.true;
+    expect(decoded.merge()).to.match(/Invalid HMAC./);
+  });
 });
